625a75b6c8f268c551db64c577a6b4118fd2414b
Nitish Goswami <itsgoswaminitish@gmail.com>
2025-05-01 06:24:45 +0000

    feat: add token validation and user ID header for post and media service proxies

--- api-gateway/src/index.js
+++ api-gateway/src/index.js
@@ -8,6 +8,7 @@ import RedisStore from "rate-limit-redis"
 import {logger} from "./utils/logger.js"
 import proxy from "express-http-proxy"
 import errorHandler from "./utils/errorHandler.js";
+import {validateToken} from "./middleware/auth.middleware.js"
 
 dotenv.config()
 
@@ -73,6 +74,53 @@ app.use(
       },
     })
   );
+
+  //setting up proxy for our post service
+app.use(
+  "/v1/posts",
+  validateToken,
+  proxy(process.env.POST_SERVICE_URL, {
+    ...proxyOptions,
+    proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
+      proxyReqOpts.headers["Content-Type"] = "application/json";
+      proxyReqOpts.headers["x-user-id"] = srcReq.user.userId;
+
+      return proxyReqOpts;
+    },
+    userResDecorator: (proxyRes, proxyResData, userReq, userRes) => {
+      logger.info(
+        `Response received from Post service: ${proxyRes.statusCode}`
+      );
+
+      return proxyResData;
+    },
+  })
+);
+
+//setting up proxy for our media service
+app.use(
+  "/v1/media",
+  validateToken,
+  proxy(process.env.MEDIA_SERVICE_URL, {
+    ...proxyOptions,
+    proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
+      proxyReqOpts.headers["x-user-id"] = srcReq.user.userId;
+      if (!srcReq.headers["content-type"].startsWith("multipart/form-data")) {
+        proxyReqOpts.headers["Content-Type"] = "application/json";
+      }
+
+      return proxyReqOpts;
+    },
+    userResDecorator: (proxyRes, proxyResData, userReq, userRes) => {
+      logger.info(
+        `Response received from media service: ${proxyRes.statusCode}`
+      );
+
+      return proxyResData;
+    },
+    parseReqBody: false,
+  })
+);
 
 
 app.use(errorHandler);
