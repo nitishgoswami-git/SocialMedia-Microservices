a295ffc28027417542683a886cb81d417a1b49da
Nitish Goswami <itsgoswaminitish@gmail.com>
2025-05-02 05:50:07 +0000

    feat: add proxy setup for search service with token validation and logging

--- api-gateway/src/index.js
+++ api-gateway/src/index.js
@@ -121,6 +121,27 @@ app.use(
     parseReqBody: false,
   })
 );
+//setting up proxy for our search service
+app.use(
+  "/v1/search",
+  validateToken,
+  proxy(process.env.SEARCH_SERVICE_URL, {
+    ...proxyOptions,
+    proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
+      proxyReqOpts.headers["Content-Type"] = "application/json";
+      proxyReqOpts.headers["x-user-id"] = srcReq.user.userId;
+
+      return proxyReqOpts;
+    },
+    userResDecorator: (proxyRes, proxyResData, userReq, userRes) => {
+      logger.info(
+        `Response received from Search service: ${proxyRes.statusCode}`
+      );
+
+      return proxyResData;
+    },
+  })
+);
 
 
 app.use(errorHandler);
